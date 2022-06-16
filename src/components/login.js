7b861ec43b350f96a262c410bdd509c80d4121ce
Sparsh Verma <2020eeb1210@iitrpr.ac.in>
2022-06-16 23:39:45 +0530

    login page changes

--- src/components/login.js
+++ src/components/login.js
@@ -5,14 +5,25 @@ const login = (props) => {
     <Container>
         <Nav>
             <a href="">
-                <img src="/images/login-logo.svg"/>
+                <img src="/images/login-logo.svg" alt=""/>
             </a>
             <div>
-                <Join>
-                    Join Now
-                </Join>
+                <Join>Join Now</Join>
+                <SignIn>Sign In</SignIn>
             </div>
         </Nav>
+        <Section>
+            <Hero>
+                <h1>Welcome to your professional community</h1>
+                <img src="images/login-hero.svg" alt=""/>
+            </Hero>
+            <Form>
+                <Google>
+                    <img alt="" src="/images/google.svg" />
+                    Sign in with Google
+                </Google>
+            </Form>
+        </Section>
     </Container>
     )
 }
@@ -42,6 +53,115 @@ const Nav = styled.nav`
 `;
 
 const Join = styled.a`
+    font-size: 16px;
+    padding: 10px 12px;
+    text-decoration: none;
+    color:rgba(0,0,0,0.6);
+    margin-right: 12px;
+    border-radius: 4px;
+    &:hover {
+        background-color: rgba(0,0,0,0.06);
+        color:rgba(0,0,0,0.9);
+        text-decoration: none;
+    }
+`;
+
+const SignIn = styled.a`
+    box-shadow: inset 0 0 0 1px #0a66c2;
+    color:#0a66c2;
+    border-radius: 24px;
+    transition-duration: 167ms;
+    font-size: 16px;
+    font-weight: 600;
+    line-height: 40px;
+    padding: 10px 24px;
+    text-align: center;
+    background-color: rgba(0,0,0,0);
+    &:hover{
+        background-color: rgba(112,181,249,0.15);
+        color:#0a66c2;
+        text-decoration: none;
+    }
+`;
+
+const Section = styled.section`
+    display: flex;
+    align-content: start;
+    min-height:700px;
+    padding-bottom:138px;
+    padding-top:40px;
+    padding:60px 0;
+    position:relative;
+    flex-wrap:wrap;
+    width:100%;
+    max-width:1128px;
+    align-items: center;
+    margin:auto;
+
+    @media(max-width:768px){
+        margin:auto;
+        min-height: 0px;
+    }
+`
+
+const Hero = styled.div`
+    width:100%;
+    h1 {
+        padding-bottom: 0;
+        width:55%;
+        font-size: 56px;
+        color:#2977c9;
+        font-weight: 200;
+        line-height: 70px;
+        @media (max-width:768px){
+            text-align:center;
+            font-size: 20px;
+            width:100%;
+            line-height: 2;
+        }
+    }
+
+    img {
+        //z-index:-1;
+        width: 700px;
+        height: 670px;
+        position:absolute;
+        bottom:-2px;
+        right: -150px;
+        @media(max-width:768px){
+            top:230px;
+            width:initial;
+            position: initial;
+            height: initial;
+        }
+    }
+`;
+
+const Form = styled.div`
+    margin-top:100px;
+    width:480px;
+    @media(max-width:768px){
+        margin-top:20px;
+    }
+`;
+
+const Google = styled.button`
+    display: flex;
+    justify-content: center;
+    background-color: #fff;
+    align-items: center;
+    height: 56px;
+    width: 100%;
+    border-radius: 28px;
+    vertical-align: middle;
+    z-index: 0;
+    transition-duration: 167ms;
+    font-size: 20px;
+    color: rgba(0,0,0,0.6);
+    &:hover{
+        background-color:rgba(207,207,207,0.25);
+        color: rgba(0,0,0,0.75);
+    }
 `;
 
 export default login;
