4b901762b596930475e28ea810a68d864965c5b2
Sparsh Verma <2020eeb1210@iitrpr.ac.in>
2022-06-22 20:17:58 +0530

    navbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
-import Login from './components/login';
+import Login from './components/Login';
+import Home from './components/Home';
+import Header from './components/Header';
 import {BrowserRouter as Router,Routes,Route} from "react-router-dom";
 
 function App() {
@@ -8,6 +10,7 @@ function App() {
       <Router>
         <Routes>
           <Route exact path="/" element={<Login/>}/>
+          <Route exact path="/home" element={<><Header/><Home/></>}/>
         </Routes>
       </Router>
     </div>
