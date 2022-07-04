e368e15c5dfb1adb22a0b51512ff99835f3279c0
Sparsh Verma <2020eeb1210@iitrpr.ac.in>
2022-07-04 12:41:15 +0530

    adding loading component

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,88 +1,106 @@
 import db, { auth, provider, storage } from "../firebase";
-import {SET_USER} from './actionType'
+import { SET_USER, SET_LOADING_STATUS } from "./actionType";
 
 export const setUser = (payload) => ({
-    type:SET_USER,
-    user:payload
-})
+  type: SET_USER,
+  user: payload,
+});
 
-export function signInAPI(){
-    return (dispatch) => {
-        auth.signInWithPopup(provider).then((payload)=>{
-            dispatch(setUser(payload.user));
-        })
-        .catch((error) => alert(error.message));
-    }
+export function setLoading(status) {
+  return {
+    type: SET_LOADING_STATUS,
+    status: status,
+  };
+}
+
+export function signInAPI() {
+  return (dispatch) => {
+    auth
+      .signInWithPopup(provider)
+      .then((payload) => {
+        dispatch(setUser(payload.user));
+      })
+      .catch((error) => alert(error.message));
+  };
 }
 
 export function getUserAuth() {
-    return (dispatch) =>{
-        auth.onAuthStateChanged(async (user)=> {
-            if(user) {
-                dispatch(setUser(user));
-            }
-        })
-    }
+  return (dispatch) => {
+    auth.onAuthStateChanged(async (user) => {
+      if (user) {
+        dispatch(setUser(user));
+      }
+    });
+  };
 }
 
 export function signOutAPI() {
-    return (dispatch) =>{
-        auth.signOut().then(()=>{
-            dispatch(setUser(null));
-        }).catch((error)=>{
-            console.log(error);
-        })
-    }
+  return (dispatch) => {
+    auth
+      .signOut()
+      .then(() => {
+        dispatch(setUser(null));
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 }
 
 export function postArticleAPI(payload) {
-	return (dispatch) => {
-		if (payload.image !== "") {
-			const upload = storage.ref(`images/${payload.image.name}`).put(payload.image);
-			upload.on(
-				"state_changed",
-				(snapshot) => {
-					const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-                    console.log(`Progress: ${progress}%`);
-                    if(snapshot.state === "RUNNING"){
-                        console.log(`Progress: ${progress}%`)
-                    }
-				},
-				(err) => alert(err),
-				async () => {
-					const downloadURL = await upload.snapshot.ref.getDownloadURL();
-					db.collection("articles").add({
-						actor: {
-							description: payload.user.email,
-							title: payload.user.displayName,
-							date: payload.timestamp,
-							image: payload.user.photoURL,
-						},
-						video: payload.video,
-						sharedImg: downloadURL,
-						comments: 0,
-						description: payload.description,
-					});
-				}
-			);
-		} else if (payload.video) {
-			db.collection("articles").add({
-				actor: {
-					description: payload.user.email,
-					title: payload.user.displayName,
-					date: payload.timestamp,
-					image: payload.user.photoURL,
-				},
-				video: payload.video,
-				sharedImg: "",
-				likes: {
-					count: 0,
-					whoLiked: [],
-				},
-				comments: 0,
-				description: payload.description,
-			});
+  return (dispatch) => {
+    dispatch(setLoading(true));
 
+    if (payload.image !== "") {
+      const upload = storage
+        .ref(`images/${payload.image.name}`)
+        .put(payload.image);
+      upload.on(
+        "state_changed",
+        (snapshot) => {
+          const progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          console.log(`Progress: ${progress}%`);
+          if (snapshot.state === "RUNNING") {
+            console.log(`Progress: ${progress}%`);
+          }
+        },
+        (err) => alert(err),
+        async () => {
+          const downloadURL = await upload.snapshot.ref.getDownloadURL();
+          db.collection("articles").add({
+            actor: {
+              description: payload.user.email,
+              title: payload.user.displayName,
+              date: payload.timestamp,
+              image: payload.user.photoURL,
+            },
+            video: payload.video,
+            sharedImg: downloadURL,
+            comments: 0,
+            description: payload.description,
+          });
+          dispatch(setLoading(false));
+        }
+      );
+    } else if (payload.video) {
+      db.collection("articles").add({
+        actor: {
+          description: payload.user.email,
+          title: payload.user.displayName,
+          date: payload.timestamp,
+          image: payload.user.photoURL,
+        },
+        video: payload.video,
+        sharedImg: "",
+        likes: {
+          count: 0,
+          whoLiked: [],
+        },
+        comments: 0,
+        description: payload.description,
+      });
+      dispatch(setLoading(false));
     }
-}
+  };
 }
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,102 +1,114 @@
 import { useState } from "react";
+import { connect } from "react-redux";
 import styled from "styled-components";
-import PostModal from "./PostModal"; 
+import PostModal from "./PostModal";
 
 const Main = (props) => {
-	
-	const [showModal,setShowModal] = useState(false);
+  const [showModal, setShowModal] = useState(false);
 
-	const clickHandler = (event) => {
-		switch(showModal){
-			case false:
-				setShowModal(true);
-				break;
-			case true:
-				setShowModal(false);
-				break;
-			default:
-				setShowModal(false);
-		}
-	}
+  const clickHandler = (event) => {
+    switch (showModal) {
+      case false:
+        setShowModal(true);
+        break;
+      case true:
+        setShowModal(false);
+        break;
+      default:
+        setShowModal(false);
+    }
+  };
 
-  return(<Container>
-        <ShareBox>
-          <div>
-            <img src="/images/user.svg" alt=""/>
-            <button onClick={clickHandler}>Click Here to Start a Post</button>
-          </div>
-          <div>
-            <button>
-              <img src="/images/photo-icon.svg" alt="" />
-              <span>Photo</span>
-            </button>
-            <button>
-              <img src="/images/video-icon.svg" alt="" />
-              <span>Video</span>
-            </button>
-            <button>
-              <img src="/images/event-icon.svg" alt="" />
-              <span>Event</span>
-            </button>
-            <button>
-              <img src="/images/article-icon.svg" alt="" />
-              <span>Write article</span>
-            </button>
-          </div>
-        </ShareBox>
+  return (
+    <Container>
+      <ShareBox>
         <div>
-          <Article>
-          <SharedActor>
-								<a>
-									<img src="/images/user.svg" alt="" />
-									<div>
-										<span>Title</span>
-										<span>Info</span>
-										<span>Date</span>
-									</div>
-								</a>
-								<button>
-									<img src="/images/ellipses.svg" alt="" />
-								</button>
-							</SharedActor>
-              <Description>
-                Description
-              </Description>
-              <SharedImg>
-              <a>
-                  <img src="/images/shared-image.jpg" alt=""/>
-                </a>
-              </SharedImg>
-              <SocialCounts> 
-                   <li>
-                        <span>80 Likes</span>
-										</li>
-										<li>
-											<a>&nbsp;&nbsp;&nbsp;&nbsp;2 comments</a>
-										</li>
-              </SocialCounts>
-              <SocialActions>
-								<button>
-                <img src="/images/like-icon.svg" alt="" />
-									<span>Like</span>
-								</button>
-								<button>
-									<img src="/images/comment-icon.svg" alt="" />
-									<span>Comment</span>
-								</button>
-								<button>
-									<img src="/images/share-icon.svg" alt="" />
-									<span>Share</span>
-								</button>
-								<button>
-									<img src="/images/send-icon.svg" alt="" />
-									<span>Send</span>
-								</button>
-							</SocialActions>
-          </Article>
-		  <PostModal showModal={showModal} clickHandler = {clickHandler}/>
+          {props.user &&
+            (props.user.photoURL ? (
+              <img src={props.user.photoURL} alt="" />
+            ) : (
+              <img src="/images/user.svg" alt="" />
+            ))}
+
+          <button
+            onClick={clickHandler}
+            disabled={props.loading ? true : false}
+          >
+            Click Here to Start a Post
+          </button>
         </div>
-    </Container>);
+        <div>
+          <button>
+            <img src="/images/photo-icon.svg" alt="" />
+            <span>Photo</span>
+          </button>
+          <button>
+            <img src="/images/video-icon.svg" alt="" />
+            <span>Video</span>
+          </button>
+          <button>
+            <img src="/images/event-icon.svg" alt="" />
+            <span>Event</span>
+          </button>
+          <button>
+            <img src="/images/article-icon.svg" alt="" />
+            <span>Write article</span>
+          </button>
+        </div>
+      </ShareBox>
+      <Content>
+        {props.loading && <img src="/images/spin-loader.gif" />}
+        <Article>
+          <SharedActor>
+            <a>
+              <img src="/images/user.svg" alt="" />
+              <div>
+                <span>Title</span>
+                <span>Info</span>
+                <span>Date</span>
+              </div>
+            </a>
+            <button>
+              <img src="/images/ellipses.svg" alt="" />
+            </button>
+          </SharedActor>
+          <Description>Description</Description>
+          <SharedImg>
+            <a>
+              <img src="/images/shared-image.jpg" alt="" />
+            </a>
+          </SharedImg>
+          <SocialCounts>
+            <li>
+              <span>80 Likes</span>
+            </li>
+            <li>
+              <a>&nbsp;&nbsp;&nbsp;&nbsp;2 comments</a>
+            </li>
+          </SocialCounts>
+          <SocialActions>
+            <button>
+              <img src="/images/like-icon.svg" alt="" />
+              <span>Like</span>
+            </button>
+            <button>
+              <img src="/images/comment-icon.svg" alt="" />
+              <span>Comment</span>
+            </button>
+            <button>
+              <img src="/images/share-icon.svg" alt="" />
+              <span>Share</span>
+            </button>
+            <button>
+              <img src="/images/send-icon.svg" alt="" />
+              <span>Send</span>
+            </button>
+          </SocialActions>
+        </Article>
+      </Content>
+      <PostModal showModal={showModal} clickHandler={clickHandler} />
+    </Container>
+  );
 };
 
 const Container = styled.div`
@@ -105,192 +117,208 @@ const Container = styled.div`
 
 const CommonCard = styled.div`
   text-align: center;
-	overflow: hidden;
-	margin-bottom: 8px;
-	background-color: #fff;
-	border-radius: 5px;
-	position: relative;
-	border: none;
-	box-shadow: 0 0 0 1px rgb(0 0 0 / 15%), 0 0 0 rgb(0 0 0 / 20%);
-`
+  overflow: hidden;
+  margin-bottom: 8px;
+  background-color: #fff;
+  border-radius: 5px;
+  position: relative;
+  border: none;
+  box-shadow: 0 0 0 1px rgb(0 0 0 / 15%), 0 0 0 rgb(0 0 0 / 20%);
+`;
 
 const ShareBox = styled(CommonCard)`
-	display: flex;
-	flex-direction: column;
-	margin: 0 0 8px;
-	color: #958b7b;
-	div {
-		button {
-			outline: none;
-			color: rgba(0, 0, 0, 0.6);
-			font-size: 14px;
-			line-height: 1.5;
-			min-height: 48px;
-			display: flex;
-			align-items: center;
-			border: none;
-			background-color: transparent;
-			font-weight: 600;
-		}
-		&:first-child {
-			display: flex;
-			align-items: center;
-			padding: 8px 16px;
-			img {
-				width: 48px;
-				border-radius: 50%;
-				margin-right: 8px;
-			}
-			button {
-				margin: 4px 0;
-				flex-grow: 1;
-				padding-left: 16px;
-				border: 1px solid rgba(0, 0, 0, 0.15);
-				border-radius: 35px;
-				text-align: left;
-			}
-		}
-		&:nth-child(2) {
-			display: flex;
-			flex-wrap: wrap;
-			justify-content: space-around;
-			padding-bottom: 4px;
-			button {
-				img {
-					margin: 0 4px 0 -2px;
-				}
-			}
-		}
-	}
+  display: flex;
+  flex-direction: column;
+  margin: 0 0 8px;
+  color: #958b7b;
+  div {
+    button {
+      outline: none;
+      color: rgba(0, 0, 0, 0.6);
+      font-size: 14px;
+      line-height: 1.5;
+      min-height: 48px;
+      display: flex;
+      align-items: center;
+      border: none;
+      background-color: transparent;
+      font-weight: 600;
+    }
+    &:first-child {
+      display: flex;
+      align-items: center;
+      padding: 8px 16px;
+      img {
+        width: 48px;
+        border-radius: 50%;
+        margin-right: 8px;
+      }
+      button {
+        margin: 4px 0;
+        flex-grow: 1;
+        padding-left: 16px;
+        border: 1px solid rgba(0, 0, 0, 0.15);
+        border-radius: 35px;
+        text-align: left;
+      }
+    }
+    &:nth-child(2) {
+      display: flex;
+      flex-wrap: wrap;
+      justify-content: space-around;
+      padding-bottom: 4px;
+      button {
+        img {
+          margin: 0 4px 0 -2px;
+        }
+      }
+    }
+  }
 `;
 
 const Article = styled(CommonCard)`
   padding: 0;
   margin: 0 0 8px;
   overflow: visible;
-`
+`;
 
 const SharedActor = styled.div`
-	padding-right: 40px;
-	flex-wrap: nowrap;
-	padding: 12px 16px 0;
-	margin-bottom: 8px;
-	display: flex;
-	align-items: center;
-	a {
-		margin-right: 12px;
-		flex-grow: 1;
-		overflow: hidden;
-		display: flex;
-		img {
-			width: 48px;
-			height: 48px;
-			border-radius: 50%;
-		}
-		& > div {
-			display: flex;
-			flex-direction: column;
-			flex-grow: 1;
-			flex-basis: 0;
-			margin-left: 8px;
-			overflow: hidden;
-			span {
-				text-align: left;
-				&:first-child {
-					font-size: 14px;
-					font-weight: 700;
-					color: #000;
-				}
-				&:nth-child(n + 2) {
-					font-size: 12px;
-					color: rgba(0, 0, 0, 0.6);
-				}
-			}
-		}
-	}
-	button {
-		position: absolute;
-		top: 0;
-		right: 12px;
-		border: none;
-		outline: none;
-		background: transparent;
-	}
+  padding-right: 40px;
+  flex-wrap: nowrap;
+  padding: 12px 16px 0;
+  margin-bottom: 8px;
+  display: flex;
+  align-items: center;
+  a {
+    margin-right: 12px;
+    flex-grow: 1;
+    overflow: hidden;
+    display: flex;
+    img {
+      width: 48px;
+      height: 48px;
+      border-radius: 50%;
+    }
+    & > div {
+      display: flex;
+      flex-direction: column;
+      flex-grow: 1;
+      flex-basis: 0;
+      margin-left: 8px;
+      overflow: hidden;
+      span {
+        text-align: left;
+        &:first-child {
+          font-size: 14px;
+          font-weight: 700;
+          color: #000;
+        }
+        &:nth-child(n + 2) {
+          font-size: 12px;
+          color: rgba(0, 0, 0, 0.6);
+        }
+      }
+    }
+  }
+  button {
+    position: absolute;
+    top: 0;
+    right: 12px;
+    border: none;
+    outline: none;
+    background: transparent;
+  }
 `;
 
 const Description = styled.div`
-	padding: 0 16px;
-	overflow: hidden;
-  color: rgba(0,0,0,0.9);
-	font-size: 14px;
-	text-align: left;
+  padding: 0 16px;
+  overflow: hidden;
+  color: rgba(0, 0, 0, 0.9);
+  font-size: 14px;
+  text-align: left;
 `;
 const SharedImg = styled.div`
-	margin-top:8px;
-  width:100%;
+  margin-top: 8px;
+  width: 100%;
   display: block;
-  position:relative;
-  background-color:#f9fafb;
+  position: relative;
+  background-color: #f9fafb;
   img {
-    object-fit:contain;
-    width:100%;
-    height:100%;
+    object-fit: contain;
+    width: 100%;
+    height: 100%;
+  }
+`;
+
+const Content = styled.div`
+  text-align: center;
+  & > img {
+    width: 30px;
   }
 `;
 
 const SocialCounts = styled.ul`
-	line-height: 1.3;
-	display: flex;
-	align-items: flex-start;
-	overflow: auto;
-	margin: 0 16px;
-	padding: 8px 0;
-	border-bottom: 1px solid #e9efdf;
-	color: rgba(0, 0, 0, 0.6);
-	list-style: none;
-	li {
-		margin-right: 5px;
-		font-size: 12px;
-		button {
-			display: flex;
-			border: none;
-			color: rgba(0, 0, 0, 0.6);
-			background: transparent;
-			span {
-				padding-left: 5px;
-			}
-		}
-	}
+  line-height: 1.3;
+  display: flex;
+  align-items: flex-start;
+  overflow: auto;
+  margin: 0 16px;
+  padding: 8px 0;
+  border-bottom: 1px solid #e9efdf;
+  color: rgba(0, 0, 0, 0.6);
+  list-style: none;
+  li {
+    margin-right: 5px;
+    font-size: 12px;
+    button {
+      display: flex;
+      border: none;
+      color: rgba(0, 0, 0, 0.6);
+      background: transparent;
+      span {
+        padding-left: 5px;
+      }
+    }
+  }
 `;
 
 const SocialActions = styled.div`
-	display: flex;
-	align-items: center;
-	justify-content: flex-start;
-	margin: 4px 12px;
-	min-height: 40px;
-	padding-bottom: 5px;
-	button {
-		display: inline-flex;
-		align-items: center;
-		padding: 8px;
-		border: none;
-		background: transparent;
-		span {
-			margin-left: 4px;
-			color: rgba(0, 0, 0, 0.6);
-			font-size: 14px;
-		}
-	}
-	button.active {
-		span {
-			color: #0a66c2;
-			font-weight: 600;
-		}
-		svg {
-			fill: #0a66c2;
-		}
-	}
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  margin: 4px 12px;
+  min-height: 40px;
+  padding-bottom: 5px;
+  button {
+    display: inline-flex;
+    align-items: center;
+    padding: 8px;
+    border: none;
+    background: transparent;
+    span {
+      margin-left: 4px;
+      color: rgba(0, 0, 0, 0.6);
+      font-size: 14px;
+    }
+  }
+  button.active {
+    span {
+      color: #0a66c2;
+      font-weight: 600;
+    }
+    svg {
+      fill: #0a66c2;
+    }
+  }
 `;
 
-export default Main;
+const mapStateToProps = (state) => {
+  return {
+    user: state.userState.user,
+    loading: state.articleState.loading,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => ({})
+
+export default connect(mapStateToProps,mapDispatchToProps)(Main);
